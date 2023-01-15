bac17f69e623106c17b54a49f9caa16d07f2bed8
Geckonda <ya.lozhin2014@yandex.ru>
2023-01-15 14:53:26 +0400

    Starter pack

---
+++ PlotCreator/StartUpInit.cs
@@ -0,0 +1,13 @@
+﻿namespace PlotCreator
+{
+    public static class StartUpInit
+    {
+        public static void InitialiseRepositories(this IServiceCollection services)
+        {
+        }
+
+        public static void InitialiseServices(this IServiceCollection services)
+        {
+        }
+    }
+}
