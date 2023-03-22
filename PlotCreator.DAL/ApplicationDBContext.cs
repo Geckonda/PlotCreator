9f3a59821150bf67f7e67d7610fc3f47c9ac053e
OblivionSorcerer <aquariusstd@yandex.ru>
2023-03-22 21:27:39 +0400

    Добавление DBContext

---
+++ PlotCreator.DAL/ApplicationDBContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlotCreator.Domain.Entity;
+using PlotCreator.Domain.Entity.Multiple_Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotCreator.DAL
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Role> Roles => Set<Role>();
+        public DbSet<Access_Modificator> Modificators => Set<Access_Modificator>();
+        public DbSet<Rating> Ratings => Set<Rating>();
+        public DbSet<Genre> Genres => Set<Genre>();
+        public DbSet<Book_Status> Statuses => Set<Book_Status>();
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Idea> Ideas => Set<Idea>();
+        public DbSet<Book_Idea> Books_Ideas => Set<Book_Idea>();
+        public DbSet<Character> Characters => Set<Character>();
+        public DbSet<Episode> Episodes => Set<Episode>();
+        public DbSet<Event> Events => Set<Event>();
+        public DbSet<Group> Groups => Set<Group>();
+        public DbSet<Book_Character> Books_Characters => Set<Book_Character>();
+        public DbSet<Episode_Event> Episodes_Events => Set<Episode_Event>();
+        public DbSet<Episode_Character> Episodes_Characters => Set<Episode_Character>();
+        public DbSet<Event_Character> Events_Characters => Set<Event_Character>();
+        public DbSet<Group_Character> Groups_Characters => Set<Group_Character>();
+        public DbSet<Group_Event> Groups_Events => Set<Group_Event>();
+
+        public ApplicationDBContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MigrationsTest;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
