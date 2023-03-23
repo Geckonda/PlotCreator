d386b66eb0f255d8c10df2d13b8ae1fe9a076292
geckonda <ya.lozhin2014@yandex.ru>
2023-03-24 00:22:22 +0400

    Добавление StatusCode

---
+++ PlotCreator.Domain/Enum/StatusCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotCreator.Domain.Enum
+{
+    public enum StatusCode
+    {
+        Ok = 200,//Все хорошо
+        NotFound = 404,//Страница не найдена
+        InternalServerError = 500//Внутренняя ошибка сервера
+    }
+}
