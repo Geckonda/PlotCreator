679307845a315e695915d957efda7c4d7ef27075
geckonda <ya.lozhin2014@yandex.ru>
2023-03-24 00:35:25 +0400

    Добавление BookViewModel

---
+++ PlotCreator.Domain/ViewModels/BookViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using PlotCreator.Domain.Entity.Multiple_Tables;
+using PlotCreator.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotCreator.Domain.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } 
+        public int Access_ModificatorId { get; set; } 
+        public string? Title { get; set; }
+        public int RatingId { get; set; }
+        public int GenreId { get; set; }
+        public int Book_StatusId { get; set; } 
+        public string? Description { get; set; }
+        public string? Book_cover { get; set; }
+    }
+}
