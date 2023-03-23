b091b8cf5853eeef52763ca5760cc77f3359f33c
geckonda <ya.lozhin2014@yandex.ru>
2023-03-24 00:47:59 +0400

    Добавление IBookService

---
+++ PlotCreator.Service/Interfaces/IBookService.cs
@@ -0,0 +1,20 @@
+﻿using PlotCreator.Domain.Entity;
+using PlotCreator.Domain.Response.Interfaces;
+using PlotCreator.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotCreator.Service.Interfaces
+{
+    public interface IBookService
+    {
+        Task<IBaseResponse<IEnumerable<Book>>> GetBooks();
+        Task<IBaseResponse<BookViewModel>> GetBook(int id);
+        Task<IBaseResponse<BookViewModel>> CreateBook(BookViewModel book);
+        Task<IBaseResponse<Book>> EditBook (int? id, BookViewModel book);
+        Task<IBaseResponse<bool>> DeleteBook (int id);
+    }
+}
