072ff3a7602db526526cca2266fd1901699e9013
geckonda <ya.lozhin2014@yandex.ru>
2023-03-24 00:17:16 +0400

    Добавление  IBaseRepository

---
+++ PlotCreator.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotCreator.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<bool> Add(T entity);
+        Task<bool> Delete(T entity);
+        Task<T> Update(T entity);
+        IQueryable<T> GetAll();
+    }
+}
